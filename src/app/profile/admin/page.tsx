c1262421ba1eb287a542f1ceb54ff9c3917b82cb
Meiring van Niekerk <meiring0505@gmail.com>
2025-09-10 20:28:03 +0200

    Attempted adding account page

--- src/app/profile/admin/page.tsx
+++ src/app/profile/admin/page.tsx
@@ -3,8 +3,10 @@ import AdminItemsTable from "./admin-items-table";
 import { Breadcrumbs } from "@/components/ui/breadcrumb";
 
 export default async function AdminDashboard({
+    account,
     searchParams
 }: {
+    account: React.ReactNode;
     searchParams?: Promise<any>;
 }) {
     const searchParamsValue = await searchParams;
@@ -15,7 +17,7 @@ export default async function AdminDashboard({
             <Tabs defaultValue="dashboard" className="w-full">
                 <TabsList className="mb-4">
                     <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
-                    <TabsTrigger value="profile">Profile</TabsTrigger>
+                    <TabsTrigger value="profile">Account</TabsTrigger>
                     <TabsTrigger value="sale">Sale</TabsTrigger>
                 </TabsList>
 
@@ -25,8 +27,8 @@ export default async function AdminDashboard({
                         page={searchParamsValue?.page ? parseInt(searchParamsValue.page) : 1}
                     />                  
                 </TabsContent>
-                <TabsContent value="profile">
-                    <div>Hello there</div>
+                <TabsContent value="account">
+                    {account}
                 </TabsContent>
                 <TabsContent value="sale">
                     <div>Hello there</div>
@@ -35,3 +37,30 @@ export default async function AdminDashboard({
         </>            
     )
 }
+
+// export default function Layout({
+//     children,
+//     profile,
+//     sale
+// }: {
+//     children: React.ReactNode;
+//     profile: React.ReactNode;
+//     sale: React.ReactNode;
+
+// }){
+//   return (
+//     <div className="max-w-screen-lg mx-auto px-4 py-10">
+//       <Tabs defaultValue="dashboard" className="w-full">
+//         <TabsList className="mb-4">
+//           <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
+//           <TabsTrigger value="profile">Profile</TabsTrigger>
+//           <TabsTrigger value="sale">Sale</TabsTrigger>
+//         </TabsList>
+
+//         <TabsContent value="dashboard">{children}</TabsContent>
+//         <TabsContent value="profile">{profile}</TabsContent>
+//         <TabsContent value="sale">{sale}</TabsContent>
+//       </Tabs>
+//     </div>
+//   );
+// }
